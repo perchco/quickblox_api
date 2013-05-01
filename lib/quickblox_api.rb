14d1c6037a29028f6f20c96cdf323e729fd3f7cb
Adam Stanton <adamstanton1979@gmail.com>
2013-05-01 14:42:40 -0700

    Add api call to sign up a user.

--- lib/quickblox_api.rb
+++ lib/quickblox_api.rb
@@ -15,9 +15,23 @@ module QuickbloxApi
     get_request "https://#{self.domain}/users/external/#{external_id}.json"
   end
 
+  def self.user_sign_up(user_attributes, options={})
+    post_request "https://#{self.domain}/users.json", user_attributes
+  end
+
   def self.get_request(url)
-    response = RestClient.get url, "QB-Token" => self.session_token
+    response = RestClient.get url, token_header
     JSON.parse(response)
   end
+
+  def self.post_request(url, params={})
+    response = RestClient.post url, params, token_header
+    JSON.parse(response)
+  end
+
+  private
+    def self.token_header
+      { "QB-Token" => self.session_token }
+    end
 end
 
